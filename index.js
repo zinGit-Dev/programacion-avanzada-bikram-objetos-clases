6410dd376ed83aa18b68f2e892c14dcd51b1c935
ZinGit-Dev <zei.informa@gmail.com>
2020-10-25 04:44:34 +0100

    ejercicios hasta el numero 16

--- index.js
+++ index.js
@@ -71,6 +71,8 @@ const Coche = {
         }
         };
 
+        // 10.- Crea la clase Perrazo con las propiedades: nombre, raza y el método popo {devuelve ("Ha hecho " + Math.random() * 3 + " caquitas")}
+        
         class Perrazo {
             constructor(nombre, raza){
               this.nombre=nombre
@@ -80,3 +82,80 @@ const Coche = {
                 return "Ha hecho "+ Math.random() * 3 + " caquitas"
               };
           };
+
+        //  11- Crea una nueva instancia de Perrazo llamada lebrelazo cuyas propiedades sean: nombre: "Gus", raza: "Lebrel"
+
+        const lebrelazo = new Perrazo("Gus","Lebrel")
+
+        
+// Nivel intermedio
+    // 12.- Crea la clase Husky que extienda la clase Perrazo (class Husky extends Perrazo) y añade la propiedad: heterocromia (booleano), 
+    // la propiedad raza siempre será "Husky" y el método tieneHeterocromia (imprime por console "si" o "no" en base a la propiedad heterocromia)
+
+    class Husky extends Perrazo {
+        
+        constructor(nombre, raza, heterocromia){
+          super(nombre,raza)
+          this.heterocromia = true
+          this.raza= "Husky"
+        }
+        tieneHeterocromia(){
+            if(this.heterocromia=== true){
+            console.log("si");
+            } else {
+            console.log("no");
+            }
+        }
+      }
+
+    //   13.- Crea una nueva instancia de Husky llamada miHusky cuyas propiedades sean: nombre: "Alaska", "heterocromia": true
+
+     const miHusky= new Husky("Alaska","Husky","heterocromia")
+
+    //  14.- Crea la clase Coordenadas con las propiedades: x, y
+
+        class Coordenadas {
+            constructor(x,y){
+                this.x=x
+                this.y=y
+            }
+        }
+
+    // 15.- Crea la clase Jugador con las propiedades: posicion (instancia de coordenadas), fuerza, velocidad (instancia de coordenadas), 
+    // direccion (instancia de coordenadas) vida y
+    //  los métodos: moverse {posicion.x += velocidad.x * direccion.x, posicion.y += velocidad.y * direccion.y}, perderVida {vida -= 1}
+
+        class Jugador {
+
+            constructor(posicion,fuerza,velocidad,direccion,vida){
+                this.posicion= posicion
+                this.fuerza= fuerza
+                this.velocidad= velocidad
+                this.direccion= direccion
+                this.vida=vida
+            }
+            moverse(x,y){
+                
+                posicion.x += velocidad.x * direccion.x
+                posicion.y += velocidad.y * direccion.y
+            }
+            perderVida(){
+                vida -= 1
+            }
+        }
+
+        // 16.- Crea un array de instancias de Jugador llamado jugadores
+        const jugadores= []
+        jugadores = new Jugador (posicion, fuerza, velocidad, direccion, vida)
+
+        // 17.- Crea el objeto FrameworksJavaScript con la propiedad: array frameworks y
+        //  el método: generarFramework {añade al array frameworksJavaScript el string (randomString() + ".js")}
+
+        const FrameworksJavaScript = {
+            frameworks: [],
+
+            generalFramework: function(){
+                const frameworksJavaScript =[]
+                 frameworksJavaScript.push(randomString() + ".js");
+            }
+        };
